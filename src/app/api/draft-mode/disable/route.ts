91f38afb9c1e074a5bd1ab1c54afbac71888313b
Artovnia <sayuri.platform@gmail.com>
2026-05-17 11:23:51 +0200

    draft mode fix

--- src/app/api/draft-mode/disable/route.ts
+++ src/app/api/draft-mode/disable/route.ts
@@ -1,9 +1,14 @@
+import { perspectiveCookieName } from '@sanity/preview-url-secret/constants'
+import { cookies } from 'next/headers'
 import { draftMode } from 'next/headers'
 import { redirect } from 'next/navigation'
 
 export async function GET(request: Request) {
   const draft = await draftMode()
   draft.disable()
+
+  const cookieStore = await cookies()
+  cookieStore.delete(perspectiveCookieName)
   
   const url = new URL(request.url)
   const redirectTo = url.searchParams.get('redirectTo') || '/'
